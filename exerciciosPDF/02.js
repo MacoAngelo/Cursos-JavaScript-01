fc860124e34e413c95cee91d343b1742b1d83d95
MacoAngelo <marco.angelo.blu@gmail.com>
2020-02-05 21:46:30 -0300

    Exercicios

--- exerciciosPDF/02.js
+++ exerciciosPDF/02.js
@@ -1,1 +1,16 @@
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+    if (ladoA === ladoB && ladoB === ladoC) {
+        console.log('Triângulo Equilátero')
+    } else if (ladoA === ladoB || ladoB === ladoC || ladoC === ladoA) {
+        console.log('Triângulo Isósceles')
+    } else {
+        console.log('Triângulo Escaleno')
+    }
+}
 
+
+classificaTriangulo(10, 10, 10);
+classificaTriangulo(10, 10, 0);
+classificaTriangulo(10, 0, 10);
+classificaTriangulo(0, 10, 10);
+classificaTriangulo(8, 12, 10);
