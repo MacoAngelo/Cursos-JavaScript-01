7315c705d8012dedfa1e077638a16f81b30a8116
MacoAngelo <marco.angelo.blu@gmail.com>
2020-02-05 21:25:57 -0300

    Final aula 86

---
+++ funcao/desafioFuncaoConstrutora.js
@@ -0,0 +1,7 @@
+function Pessoa (nome) {
+    this.nome = nome
+    this.falar = () => console.log(`Meu nome é ${nome}`)
+}
+
+const p1 = new Pessoa('Marco')
+p1.falar()
---
+++ funcao/factory1.js
@@ -0,0 +1,9 @@
+// Factory simples
+function criarPessoa() {
+    return{
+        nome: 'Marco',
+        sobrenome: 'Angelo'
+    }
+}
+
+console.log(criarPessoa())
