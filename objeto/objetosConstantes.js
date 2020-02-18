93594f9ed2336c67c826a91d4195e068bb916dfa
MacoAngelo <marco.angelo.blu@gmail.com>
2020-02-18 20:49:08 -0300

    Att

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,21 @@
+// Pessoa -> 123 -> {...}
+const pessoa = {nome: 'Marco'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// pessoa <- 456 <- {...}
+//pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({nome: 'Marco'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
